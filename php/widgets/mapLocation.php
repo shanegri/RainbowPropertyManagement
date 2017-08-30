b5b1cfb1c710f615d0f97638fa207f78972ce192
Shane <shgriffin16@gmail.com>
2017-08-30 14:46:00 -0400

    Added link to directions

--- php/widgets/mapLocation.php
+++ php/widgets/mapLocation.php
@@ -7,6 +7,11 @@
     $city = $prop->v('city');
     $zip = $prop->v('zip');
     $search = $address . " " . $city ." ". $zip;
+    $directionLink = str_replace(" ", "+", $address) . "+,"
+                   . str_replace(" ", "+", $city) ."+,".
+                     $zip;
+    echo '
+    <a target="_blank" style="text-decoration: none"href="https://www.google.com/maps/place/'.$directionLink.'"><h2 class="text-center" style="font-size: 20px; margin: 2px;"><small style="color: white; ">Link To Directions</small></h2></a>';
   } else {
     $search = '1514 Main st. Niagara Falls NY';
     echo '<h2 class="text-center" style="font-size: 20px; margin: 2px;"><small style="color: white; ">Our Location</small></h2>';
