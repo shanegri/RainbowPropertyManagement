2b8947d199e1c38a1146517f7d736f9cec00fe80
Shane <shgriffin16@gmail.com>
2017-10-23 16:18:24 -0400

    Lic plate numberform input now str

--- classes/AppForms/Vehicle.php
+++ classes/AppForms/Vehicle.php
@@ -7,12 +7,12 @@ class Vehicle extends Form implements iIncrementalForm {
   private $id;
 
   public function __construct($id){
-      parent::__construct("ApplicationForm"); 
+      parent::__construct("ApplicationForm");
       $this->id = $id .get_class($this);
       $this->addInput("vehMakeModel".$this->id, "Make/Model", FormInput::$STR, 20, true);
       $this->addInput("vehYear".$this->id, "Year", FormInput::$INT, 4, true);
       $this->addInput("vehColor".$this->id, "Color", FormInput::$STR, 20, true);
-      $this->addInput("vehTag".$this->id, "Tag No./State", FormInput::$INT, 20, true);
+      $this->addInput("vehTag".$this->id, "Tag No./State", FormInput::$STR, 20, true);
   }
   public function showForm(){
     echo '<div class="row">';
